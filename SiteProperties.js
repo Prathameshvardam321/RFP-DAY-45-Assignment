95618ab1dd176f29ae27e7f1cc1e8d7e5153631c
Prathameshvardam321 <prathameshvardam321@gmail.com>
2022-12-05 23:33:14 +0530

    Added EmployePayrollHomePageUC6

--- SiteProperties.js
+++ SiteProperties.js
@@ -1,4 +1,4 @@
 let site_properties = {
-    home_page : "/new_home.js",
-    add_emp_payroll_page: "/EmployeePayroll.js"
+    home_page : "../EmployeePayrollHomePage/new_homepage.js",
+    add_emp_payroll_page: "../EmployeePayrollHomePage/EmployeePayroll.js"
 };
--- new_homepage.js
+++ new_homepage.js
@@ -1,6 +1,13 @@
-wwindow.addEventListener('DOMContentLoaded', (event) => {
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>" + 
@@ -43,7 +50,7 @@ const createEmployeePayrollJSON = () => {
                 'Engineering',
                 'Finance'
             ],
-            _salary:'500000',
+            _salary:'900000',
             _startDate: '29 Oct 2019',
             _note:'',
             _id: new Date().getTime(),
